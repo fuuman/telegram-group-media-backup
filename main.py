aea98268315d66a6a04f4e04a806dd633e824a1a
Marco Schanz <mschanz88@web.de>
2017-06-11 13:34:26 +0200

    add requirements; some fixes

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
 
-from telegram.ext import Updater, CommandHandler, Job, MessageHandler, Filters
+from telegram.ext import Updater, MessageHandler, Filters
 import logging
 import configparser
 import datetime
-import time
+import os
 from lib.owncloud_helper import OwncloudHelper
 from owncloud import HTTPResponseError
 
@@ -37,26 +37,41 @@ oc = OwncloudHelper().oc
 
 def backup(bot, update):
     message = update.message
+    now = str (datetime.datetime.now())
+    for c in ['.', ':', '-', ' ']:
+        now = now.replace(c, '_')
     if len(message.photo) > 0:
         file_id = message.photo[-1].file_id
-        file_name = 'Photo_{id}'.format(id=str(time.time()).replace('.',''))
+        file_name = 'Photo_{now}'.format(now=now)
     elif message.video is not None:
         file_id = message.video.file_id
-        file_name = 'Video_{id}'.format(id=str(time.time()).replace('.',''))
+        file_name = 'Video_{now}'.format(now=now)
     elif message.document is not None:
         file_id = message.document.file_id
         file_name = message.document.file_name
 
-    bot.get_file(file_id).download(custom_path="/tmp/{file_name}".format(file_name=file_name))
+    local_tmp_file = "/tmp/{file_name}".format(file_name=file_name)
+    bot.get_file(file_id).download(custom_path=local_tmp_file)
+
+    try:
+        oc.mkdir(config['owncloud']['BACKUP_FOLDER'])
+    except:
+        logger.info('{} already exists.'.format(config['owncloud']['BACKUP_FOLDER']))
 
     today = str(datetime.date.today())
     try:
-        oc.mkdir('family_media_backup/{}'.format(today))
+        oc.mkdir('{backup_folder}/{today}'.format(backup_folder=config['owncloud']['BACKUP_FOLDER'],
+                                                  today=today))
     except HTTPResponseError:
         pass
 
-    oc.put_file('family_media_backup/{}/{}'.format(today, file_name),
-                '/tmp/{file_name}'.format(file_name=file_name))
+    oc.put_file('{backup_folder}/{today}/{file_name}'.format(backup_folder=config['owncloud']['BACKUP_FOLDER'],
+                                                             today=today,
+                                                             file_name=file_name), local_tmp_file)
+
+    os.remove(local_tmp_file)
+
+    logger.info('File {} backuped to owncloud.'.format(file_name))
 
 def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"' % (update, error))
