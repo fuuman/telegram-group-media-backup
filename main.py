285d9af6165990fb074234463a3a8ffe3fdff714
Marco Schanz <mschanz@uni-koblenz.de>
2017-06-11 13:35:48 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,21 +1,4 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# Simple Bot to send timed Telegram messages
-# This program is dedicated to the public domain under the CC0 license.
-"""
-This Bot uses the Updater class to handle the bot and the JobQueue to send
-timed messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Alarm Bot example, sends a message after a set time.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
 
 from telegram.ext import Updater, MessageHandler, Filters
 import logging
